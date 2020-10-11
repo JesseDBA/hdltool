4275cc438728c52f0c39b624dafa8ffc8aee1e2c
吴昊 <wuhao@en-core.com.cn>
2020-10-11 23:57:18 +0800

    add first html

--- src/main/java/com/hdl/hdltool/controller/ApiController.java
+++ src/main/java/com/hdl/hdltool/controller/ApiController.java
@@ -1,10 +1,12 @@
 package com.hdl.hdltool.controller;
 
+import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpUtil;
 import com.alibaba.fastjson.JSON;
 import com.hdl.hdltool.config.MyProps;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -24,13 +26,47 @@ public class ApiController {
     private MyProps myProps;
 
     @GetMapping("/app/cityPosition/getCityList")
-    public String getCityList(){
+    public String getCityList() {
 
-        Map<String,Object> paramsMap = new HashMap<String,Object>();
-        paramsMap.put("accessToken","at.123456123456");
+        Map<String, Object> paramsMap = new HashMap<String, Object>();
+        paramsMap.put("accessToken", "at.123456123456");
         String result = "";
-        result = HttpUtil.post(myProps.getUrl()+"/app/cityPosition/getCityList",paramsMap);
-        com.alibaba.fastjson.JSONObject jsonResult= JSON.parseObject(result);
+        result = HttpUtil.post(myProps.getUrl() + "/app/cityPosition/getCityList", paramsMap);
+        com.alibaba.fastjson.JSONObject jsonResult = JSON.parseObject(result);
+        System.out.println(jsonResult.toString());
+        return jsonResult.toString();
+    }
+
+    @GetMapping(value = "/app/v2/getStoreListByCityId",params = "cityId")
+    public String getStoreListByCityId(@RequestParam String cityId) {
+
+        Map<String, Object> paramsMap = new HashMap<String, Object>();
+        paramsMap.put("cityId", cityId);
+
+        String paramsJson = JSON.toJSONString(paramsMap);
+        String result = "";
+
+        result = HttpRequest.post(myProps.getUrl() + "/app/v2/getStoreListByCityId")
+                .body(paramsJson)
+                .execute().body();
+        com.alibaba.fastjson.JSONObject jsonResult = JSON.parseObject(result);
+        System.out.println(jsonResult.toString());
+        return jsonResult.toString();
+    }
+
+    @GetMapping(value = "/app/v2/getQueueStatus",params = "storeId")
+    public String getQueueStatus(@RequestParam String storeId) {
+
+        Map<String, Object> paramsMap = new HashMap<String, Object>();
+        paramsMap.put("storeId", storeId);
+
+        String paramsJson = JSON.toJSONString(paramsMap);
+        String result = "";
+
+        result = HttpRequest.post(myProps.getUrl() + "/app/v2/getQueueStatus")
+                .body(paramsJson)
+                .execute().body();
+        com.alibaba.fastjson.JSONObject jsonResult = JSON.parseObject(result);
         System.out.println(jsonResult.toString());
         return jsonResult.toString();
 //        JSONArray ja = jsonResult.getJSONArray("data");
