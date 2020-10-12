bb14c89b452146b29f7bb1f50e9988a9f5d84d1a
吴昊 <wuhao@en-core.com.cn>
2020-10-13 03:34:33 +0800

    choose city and store to get storeId

--- src/main/java/com/hdl/hdltool/controller/ApiController.java
+++ src/main/java/com/hdl/hdltool/controller/ApiController.java
@@ -1,15 +1,20 @@
 package com.hdl.hdltool.controller;
 
-import cn.hutool.http.HttpRequest;
-import cn.hutool.http.HttpUtil;
+import cn.hutool.core.map.MapUtil;
 import com.alibaba.fastjson.JSON;
-import com.hdl.hdltool.config.MyProps;
+import com.alibaba.fastjson.JSONObject;
+import com.hdl.hdltool.service.ApiService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.HashMap;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,62 +27,82 @@ import java.util.Map;
 @RestController
 public class ApiController {
 
+//    @Autowired
+//    private MyProps myProps;
+//
+//    @GetMapping("/app/cityPosition/getCityList")
+//    public String getCityList() {
+//
+//        Map<String, Object> paramsMap = new HashMap<String, Object>();
+//        paramsMap.put("accessToken", "at.123456123456");
+//        String result = "";
+//        result = HttpUtil.post(myProps.getUrl() + "/app/cityPosition/getCityList", paramsMap);
+//        com.alibaba.fastjson.JSONObject jsonResult = JSON.parseObject(result);
+//        System.out.println(jsonResult.toString());
+//        return jsonResult.toString();
+//    }
+//
+//    @GetMapping(value = "/app/v2/getStoreListByCityId",params = "cityId")
+//    public String getStoreListByCityId(@RequestParam String cityId) {
+//
+//        Map<String, Object> paramsMap = new HashMap<String, Object>();
+//        paramsMap.put("cityId", cityId);
+//
+//        String paramsJson = JSON.toJSONString(paramsMap);
+//        String result = "";
+//
+//        result = HttpRequest.post(myProps.getUrl() + "/app/v2/getStoreListByCityId")
+//                .body(paramsJson)
+//                .execute().body();
+//        com.alibaba.fastjson.JSONObject jsonResult = JSON.parseObject(result);
+//        System.out.println(jsonResult.toString());
+//        return jsonResult.toString();
+//    }
+//
+//    @GetMapping(value = "/app/v2/getQueueStatus",params = "storeId")
+//    public String getQueueStatus(@RequestParam String storeId) {
+//
+//        Map<String, Object> paramsMap = new HashMap<String, Object>();
+//        paramsMap.put("storeId", storeId);
+//
+//        String paramsJson = JSON.toJSONString(paramsMap);
+//        String result = "";
+//
+//        result = HttpRequest.post(myProps.getUrl() + "/app/v2/getQueueStatus")
+//                .body(paramsJson)
+//                .execute().body();
+//        com.alibaba.fastjson.JSONObject jsonResult = JSON.parseObject(result);
+//        System.out.println(jsonResult.toString());
+//        return jsonResult.toString();
+////        JSONArray ja = jsonResult.getJSONArray("data");
+////        for(int i = 0; i < ja.size(); i++){
+////            com.alibaba.fastjson.JSONObject jo = ja.getJSONObject(i);
+////            System.out.println(jo.getString("deviceSerial"));
+////            if(deviceSerial.equals(jo.getString("deviceSerial"))){
+////                return jo.getString("rtmp");
+////            }
+////        }
+//
+//    }
+
     @Autowired
-    private MyProps myProps;
+    ApiService apiService;
 
-    @GetMapping("/app/cityPosition/getCityList")
-    public String getCityList() {
-
-        Map<String, Object> paramsMap = new HashMap<String, Object>();
-        paramsMap.put("accessToken", "at.123456123456");
-        String result = "";
-        result = HttpUtil.post(myProps.getUrl() + "/app/cityPosition/getCityList", paramsMap);
-        com.alibaba.fastjson.JSONObject jsonResult = JSON.parseObject(result);
-        System.out.println(jsonResult.toString());
-        return jsonResult.toString();
+    @RequestMapping("/getStore")
+    public ModelAndView getStore() {
+        return new ModelAndView("getStore.html");
     }
 
-    @GetMapping(value = "/app/v2/getStoreListByCityId",params = "cityId")
-    public String getStoreListByCityId(@RequestParam String cityId) {
-
-        Map<String, Object> paramsMap = new HashMap<String, Object>();
-        paramsMap.put("cityId", cityId);
-
-        String paramsJson = JSON.toJSONString(paramsMap);
-        String result = "";
-
-        result = HttpRequest.post(myProps.getUrl() + "/app/v2/getStoreListByCityId")
-                .body(paramsJson)
-                .execute().body();
-        com.alibaba.fastjson.JSONObject jsonResult = JSON.parseObject(result);
-        System.out.println(jsonResult.toString());
-        return jsonResult.toString();
+    @RequestMapping(value = "/getCityList", method = RequestMethod.GET)
+    public void getCityList(HttpServletResponse httpServletResponse) throws IOException {
+        Map<String, List<Map<Object, Object>>> cityList = apiService.getCityList();
+        httpServletResponse.setHeader("Content-Encoding", "UTF-8");
+        httpServletResponse.setContentType("application/json;charset=UTF-8");
+        httpServletResponse.getWriter().print(JSON.toJSONString(MapUtil.sort(cityList)));
     }
 
-    @GetMapping(value = "/app/v2/getQueueStatus",params = "storeId")
-    public String getQueueStatus(@RequestParam String storeId) {
-
-        Map<String, Object> paramsMap = new HashMap<String, Object>();
-        paramsMap.put("storeId", storeId);
-
-        String paramsJson = JSON.toJSONString(paramsMap);
-        String result = "";
-
-        result = HttpRequest.post(myProps.getUrl() + "/app/v2/getQueueStatus")
-                .body(paramsJson)
-                .execute().body();
-        com.alibaba.fastjson.JSONObject jsonResult = JSON.parseObject(result);
-        System.out.println(jsonResult.toString());
-        return jsonResult.toString();
-//        JSONArray ja = jsonResult.getJSONArray("data");
-//        for(int i = 0; i < ja.size(); i++){
-//            com.alibaba.fastjson.JSONObject jo = ja.getJSONObject(i);
-//            System.out.println(jo.getString("deviceSerial"));
-//            if(deviceSerial.equals(jo.getString("deviceSerial"))){
-//                return jo.getString("rtmp");
-//            }
-//        }
-
+    @PostMapping(value = "/getStoreListByCityId", produces = "application/json;charset=utf-8")
+    public String getStoreListByCityId(@RequestBody JSONObject JSONcityId) {
+        return apiService.getStoreListByCityId(JSONcityId.getString("cityId")).toString();
     }
-
 }
---
+++ src/main/java/com/hdl/hdltool/service/ApiService.java
@@ -0,0 +1,63 @@
+package com.hdl.hdltool.service;
+
+import cn.hutool.core.collection.ListUtil;
+import cn.hutool.core.map.MapUtil;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.hdl.hdltool.config.MyProps;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * TODO
+ *
+ * @author jesse
+ * @since 2020/10/12
+ */
+
+@Service(value = "apiService")
+public class ApiService {
+
+    @Autowired
+    private MyProps myProps;
+
+    public Map<String, List<Map<Object, Object>>> getCityList() {
+        Map<String, List<Map<Object, Object>>> cityMap = MapUtil.newHashMap(true);
+        String result = HttpUtil.post(myProps.getUrl() + "/app/cityPosition/getCityList", "");
+        JSONObject jsonResult = JSON.parseObject(result);
+        JSONArray cityListJSONArray = jsonResult.getJSONObject("data").getJSONArray("cityList");
+        for (Object o : cityListJSONArray) {
+            JSONObject cityJSON = (JSONObject) o;
+            String firstLetter = cityJSON.getString("py").substring(0, 1).toUpperCase();
+            Map<Object, Object> city = MapUtil.of(new String[][]{{"cityId", cityJSON.getString("cityId")}
+                    , {"cityName", cityJSON.getString("cityName")}
+                    , {"py", cityJSON.getString("py")}});
+            if (cityMap.containsKey(firstLetter)) {
+                List cityList = cityMap.get(firstLetter);
+                cityList.add(city);
+                cityMap.put(firstLetter, cityList);
+            } else {
+                cityMap.put(firstLetter, ListUtil.toLinkedList(city));
+            }
+        }
+        return cityMap;
+    }
+
+    public JSONArray getStoreListByCityId(String cityId) {
+        Map<String, Object> paramsMap = new HashMap<String, Object>();
+        paramsMap.put("cityId", cityId);
+
+        String paramsJson = JSON.toJSONString(paramsMap);
+        String result = HttpRequest.post(myProps.getUrl() + "/app/v2/getStoreListByCityId")
+                                   .body(paramsJson)
+                                   .execute().body();
+        return JSON.parseObject(result).getJSONObject("data").getJSONArray("storeList");
+    }
+}
